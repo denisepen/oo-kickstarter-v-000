b7d42232b50e0c27b04434f665ef2469d8d919ea
Denise Pendleton <dpendl1@hotmail.com>
2018-03-07 03:07:58 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,17 @@
 class Project
-    attr_accessor :title, :backer
+    attr_accessor :title, :backer, :name
+    @@all = []
+
   def initialize(title)
     @backers = []
     @title = title
+  end
+
+  def add_backer(name)
+    # p = Project.new(title)
+    # 
+    # @backers << 
+
   end
 
 end
